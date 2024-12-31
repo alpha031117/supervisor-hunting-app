bb2fe92a38391b036ef033b46683c3ea29e5b016
Alpha Chong <alphachongs@gmail.com>
2025-01-01 03:12:09 +0800

    user-report UI

--- resources/views/ManageUser/user-list.blade.php
+++ resources/views/ManageUser/user-list.blade.php
@@ -3,9 +3,9 @@
 @section('page', 'Manage User')
 
 @section('breadcrumbs')
-    <li>FYP Hunt</li>
+    <a href="{{ route('home')}}" class="hover:text-blue-600"><li>FYP Hunt</li></a>
     <li>/</li>
-    <li>Manage User</li>
+    <a href="{{ route('admin.user-list')}}" class="text-blue-600"><li>Manage User</li></a>
 @endsection
 
 @section('content')
@@ -13,9 +13,11 @@
     <div class="flex justify-between items-center mb-6">
         <h1 class="text-2xl font-semibold">List of Registered Users</h1>
         <div class="space-x-4">
-            <button class="border border-blue-500 text-blue-500 px-4 py-2 rounded-lg hover:bg-blue-100">
-                Generate Report
-            </button>
+            <a href="{{ route('admin.user-report')}}">
+                <button class="border border-blue-500 text-blue-500 px-4 py-2 rounded-lg hover:bg-blue-100">
+                    Generate Report
+                </button>
+            </a>
             <button data-modal-target="create-modal" data-modal-toggle="create-modal" class="border border-blue-500 text-blue-500 px-4 py-2 rounded-lg hover:bg-blue-100">
                 Register New User
             </button>
@@ -129,7 +131,6 @@
                       </div>
                   </div>
                   <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-                      <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
                       Create Users
                   </button>
               </form>
